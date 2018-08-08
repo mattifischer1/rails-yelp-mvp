ebad72d733af00923f97a1f57b4024e10e6d2d7a
Matti Fischer <mr.matti.fischer@googlemail.com>
2018-08-08 20:09:28 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'italian',
+    phone_number: '03030202020'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '030302025550'
+  },
+  {
+    name:         'Charlotte1',
+    address:      '54A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '030302023330'
+  },
+  {
+    name:         'Pizza West',
+    address:      '55A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '03030209990'
+  },
+  {
+    name:         'Pizza South',
+    address:      '57A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '03030277750'
+  }
+])
+
